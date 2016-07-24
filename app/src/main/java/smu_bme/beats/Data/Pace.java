f413047b622421766efc7fe5c1f5c02a110bb307
Alex Lin <Alex_vii@live.com>
2016-07-25 00:43:19 +0800

    Database Restructure

---
+++ app/src/main/java/smu_bme/beats/Data/Pace.java
@@ -0,0 +1,24 @@
+package smu_bme.beats.Data;
+
+public class Pace {
+    private int date;
+    private int spo2;
+    private int pace;
+    private int bpm;
+
+    public int getDate() {
+        return date;
+    }
+
+    public int getPace() {
+        return pace;
+    }
+
+    public int getSpo2() {
+        return spo2;
+    }
+
+    public int getBpm() {
+        return bpm;
+    }
+}
