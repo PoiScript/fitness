f885ac15c08696d9713f420cef199e9f61d4c764
Alex Lin <Alex_vii@live.com>
2016-08-03 14:57:15 +0800

    Add MarkerView and XAxisValueFormatter

---
+++ app/src/main/java/smu_bme/beats/Chart/MyMarkerView.java
@@ -0,0 +1,8 @@
+package smu_bme.beats.Chart;
+
+/**
+ * Created by alex on 8/3/2016.
+ */
+public class MyMarkerView extends com.github.mikephil.charting.components.MarkerView {
+    public MyMarkerView
+}
---
+++ app/src/main/java/smu_bme/beats/Chart/MyXAxisValueFormatter.java
@@ -0,0 +1,7 @@
+package smu_bme.beats.Chart;
+
+/**
+ * Created by alex on 8/3/2016.
+ */
+public class MyXAxisValueFormatter {
+}
